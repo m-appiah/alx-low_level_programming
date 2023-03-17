8e1efc19158cdd123d2cb3cb5df3d12fda9cbb89
Michael Appiah <mappiahca@gmail.com>
2023-03-16 22:00:27 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main A program to print a line of code using the puts function
- * Return 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
