37e6c97dd0b5a9f4a8a9ff08b8fec272e7a02e6f
Michael Appiah <mappiahca@gmail.com>
2023-03-16 21:18:00 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * A program to print a line of code using the puts function
+ *
+ * Return 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
