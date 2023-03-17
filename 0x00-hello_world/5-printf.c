06acddf1441d1da39ee7d3e61d90b02d193abee0
Michael Appiah <mappiahca@gmail.com>
2023-03-16 21:35:47 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program to print a line of code using the printf function
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
