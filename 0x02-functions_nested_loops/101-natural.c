62e5011967dc56305271bd00f45d35573401f5a8
m-appiah <mappiahca@gmail.com>
2023-03-23 05:57:47 +0000

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - print the sum to all the multiples of 3 or 5
+ * main - print the addition to all the multiples of 3 or 5
  *
  * Return:Always 0
  */
 int main(void)
 {
 	int i;
-	int sum = 0;
+	int add = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			sum = sum + i;
+			add = add + i;
 		}
 	}
-	printf("%d", sum);
+	printf("%d", add);
 	printf("\n");
 }
