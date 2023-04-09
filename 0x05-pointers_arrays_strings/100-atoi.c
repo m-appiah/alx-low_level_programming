76374ed15037d47681fd8fbc6d7eb3fbf7d3f9cd
Michael Appiah <mappiahca@gmail.com>
2023-04-09 13:51:19 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,35 +8,20 @@
  */
 int _atoi(char *s)
 {
-	int i, j, k, l, length, digit;
+	unsigned int uns = 0;
+	int i = 0, sign = 1;
 
-	i = 0;
-	j = 0;
-	k = 0;
-	l = 0;
-	length = 0;
-	digit = 0;
-
-	while (s[length] != '\0')
-		length++;
-	while (i < length && l == 0)
+	while (!(s[i] >= '0' && s[i] <= '9') && (s[i] != '\0'))
 	{
-		if (s[i] == ' ')
-			++j;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if ((j % 2) == 1)
-				digit = -digit;
-			k = k * 10 + digit;
-			l = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			l = 0;
-		}
+		if (s[i] == '-')
+			sign = sign * -1;
 		i++;
 	}
-	if (l == 0)
-		return (0);
-	return (k);
+	while ((s[i] >= '0' && s[i] <= '9') && (s[i] != '\0'))
+	{
+		uns = (s[i] - '0') + (uns * 10);
+		i++;
+	}
+	uns = uns * sign;
+	return (uns);
 }
