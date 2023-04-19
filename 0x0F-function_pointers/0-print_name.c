1f40ff6a0a4100346a456a0ae1b4973862fd479d
Michael Appiah <mappiahca@gmail.com>
2023-04-19 07:02:36 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints name
+ * @name: name to be printed
+ * @f: function pointer
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
