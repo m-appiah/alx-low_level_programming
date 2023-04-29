311eeaa722597940cd14a5d4b107647e80c69c7b
m-appiah <mappiahca@gmail.com>
2023-04-28 22:44:28 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,29 +4,37 @@
 
 /**
  * add_node - function that adds a new node at the begining of a list
- * @head: head pointer
- * @str: string
+ * @head: Double head pointer
+ * @str: string input
  *
  * Return: return the address of the element or NULL
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	/* Declare a pointer to a new list_t node */
 	list_t *newNode;
-	char *duplicateStr;
 
+	/*Allocate memory for the new node*/
 	newNode = malloc(sizeof(list_t));
+
+	/* Check if memory allocation was successful*/
 	if (newNode == NULL)
 	{
+		/* Return NULL if memory allocation fails*/
 		return (NULL);
 	}
-	duplicateStr = strdup(str);
-	if (duplicateStr == NULL)
+	/* Copy the input string and assingn it to the newNode str*/
+	newNode->str = strdup(str);
+	/* check if strdup failed */
+	if (newNode->str == NULL)
 	{
-		free(newNode);
-		return (NULL);
+		free(newNode); /* Free newNode if strdup failed */
+		return (NULL); /* Return NULL if strup failed */
 	}
-	newNode->str = duplicateStr;
+	/*Calculate the length of the input string and assign it to len field*/
+	newNode->len = strlen(str);
+	/* Assign the current head as the next node of the newNode */
 	newNode->next = *head;
-	*head = newNode;
-	return (newNode);
+	*head = newNode; /* Update the head pointer to point to the newNode */
+	return (newNode); /* Return the address of the newNode */
 }
