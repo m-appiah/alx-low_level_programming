4d644c317cc9899004f14702fde07f26fb3eed0d
m-appiah <mappiahca@gmail.com>
2023-04-30 07:25:13 -0700

    Header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
@@ -13,7 +14,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
