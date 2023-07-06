1a18ad7f2b83f213e5eb7d907a74439473669b5c
Michael Appiah <mappiahca@gmail.com>
2023-07-06 11:06:03 -0700

    Flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - return the number of flips
+ * @n: the first number to be flipped
+ * @m: the second number to be flipped
+ *
+ * Return: flipped bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int counter = 0;
+	unsigned long int b = n ^ m;
+
+	while (b)
+	{
+		counter += b & 1;
+		b >>= 1;
+	}
+	return (counter);
+}
