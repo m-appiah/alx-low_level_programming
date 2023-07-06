5924c3cda7cdf2abf6a1e965ed26c6bf40147444
Michael Appiah <mappiahca@gmail.com>
2023-07-06 10:12:30 -0700

    Number(s) to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to be converted
+ *
+ * Return: Binary numbers
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) ? '1' : '0');
+}
