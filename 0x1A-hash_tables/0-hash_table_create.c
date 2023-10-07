1a63d3dba2ea00ff22a027e9e4a00187b852cc75
Michael Appiah <mappiahca@gmail.com>
2023-10-06 18:54:55 -0700

    Write a function that creates a hash table. - Bug fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned long int i;
 
+	new_table = NULL;
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
@@ -18,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
-		/*free(new_table);*/
+		free(new_table);
 		return (NULL);
 	}
 
